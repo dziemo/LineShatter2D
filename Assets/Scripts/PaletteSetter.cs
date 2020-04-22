9d490ebfbe08dd3fe7393a44b29b14e280c814ab
Dziemo <michalek210@gmail.com>
2020-04-22 14:33:14 +0200

    Palette system ready + cleanup

---
+++ Assets/Scripts/PaletteSetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaletteSetter : MonoBehaviour
+{
+    public Material playerMat, backgroundMat, badSegmentsMat;
+
+    public List<Palette> palettes = new List<Palette>();
+
+    private void Start()
+    {
+        SetPalette();
+    }
+
+    void SetPalette ()
+    {
+        var palette = palettes[Random.Range(0, palettes.Count)];
+
+        playerMat.color = palette.playerColor;
+        backgroundMat.color = palette.backgroundColor;
+        badSegmentsMat.color = palette.badSegmentsColor;
+    }
+
+}
